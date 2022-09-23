df7b79bfc89f7fdd6db89c0227cbd3ea189fc302
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2022-09-23 20:37:43 -0300

    rota efetuar venda criada

--- routers.js
+++ routers.js
@@ -2,58 +2,25 @@ const express = require("express")
 const sequelize = require("sequelize")
 const router = express.Router()
 const produto = require("./models/Produto")
-
+const produtoController = require("./controllers/produto_controller")
+const mysql = require("mysql2")
+const conection_mysql = require("./models/connectionMysql")
+const { response } = require("express")
+const vendaController = require("./controllers/venda_controller")
+//PRODUTOS
 //adicionar produto
-
-router.post("/produto/add", async function(req, res){
-    const {Nome, Preço, Quantidade} = req.body
-    try {
-        const data = await produto.create({
-            Nome: Nome,
-            Preço: Preço,
-            Quantidade: Quantidade
-        })
-        const response = {
-            "Mensagem": "Produto adicionado",
-            "Nome": Nome,
-            "Preço": Preço,
-            "Quantidade": Quantidade,
-             Request:{
-                "Tipo": "Get",
-                "Descrição": "Retorno de todos os produtos cadastrados",
-                "URL": "http://localhost:8040/produto"
-             }
-        }
-        return res.status(200).send(response)
-    } catch (error) {
-        return res.status(404).send({error})
-    }
-})
+router.post("/produto/add", produtoController.adicionarProduto)
 //listar todos os produtos
+router.get("/produto", produtoController.listarProdutos)
+//listar produto especifico
+router.get("/produto/:id", produtoController.listarProdutoEspecifico)
+//deletar produto
+router.delete("/produto/:id", produtoController.deletarProduto)
+//atualizar produto
+router.put("/produto/:id",produtoController.atualizarProduto)
 
-router.get("/produto", async function(req, res){
-    try {
-        const data = await produto.findAll()
-        const dados = data.map(function(dad){
-            return {
-                "Mensagem" : "listagem de produtos",
-                "id" : dad.id,
-                "Nome": dad.Nome,
-                "Preço": dad.Preço,
-                "Quantidade": dad.Quantidade,
-                Request:{
-                    "Tipo": "GET",
-                    "Descrição": "listagem de produto especifico",
-                    "URL": `http://localhost:8040/produto/${dad.id}`
-                }
-            }
-        })
-        return res.status(200).send(dados) 
-    } catch (error) {
-        return res.status(404).send(error)
-    }
-    
-})
-
+//Vendas
+//venda efetuada
+router.post("/venda/add", vendaController.efetuarVenda)
 
 module.exports = router
