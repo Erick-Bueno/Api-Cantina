28377bc750f567f6395c1183af61e39d7b424d41
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2022-09-30 20:52:14 -0300

    rota de adicionar venda agora funcionando

--- routers.js
+++ routers.js
@@ -7,6 +7,7 @@ const mysql = require("mysql2")
 const conection_mysql = require("./models/connectionMysql")
 const { response } = require("express")
 const vendaController = require("./controllers/venda_controller")
+const nodemon = require("nodemon")
 //PRODUTOS
 //adicionar produto
 router.post("/produto/add", produtoController.adicionarProduto)
@@ -22,5 +23,103 @@ router.put("/produto/:id",produtoController.atualizarProduto)
 //Vendas
 //venda efetuada
 router.post("/venda/add", vendaController.efetuarVenda)
+
+//teste array de produtos estoque
+
+router.post("/vendas/est", async function(req, res){
+
+    const produtoss = req.body
+    let vendas = []
+    let lucrototal = 0
+    let preço_item = 0
+    
+
+    for(let i = 0; i < produtoss.produtos.length; i = i + 1){
+        function verificar_estoque(){
+            return new Promise((resolve, reject) => {
+                conection_mysql.con.query(`select * from produtos where id = ${produtoss.produtos[i].idProduto}`, function(error, results){
+                    if(error){
+                        return reject(error)
+    
+                    }return resolve(results)
+                })
+            })
+        }let dado = await verificar_estoque() 
+        console.log(dado)  
+        if(dado.length == 0){
+           return res.status(400).send("produto inexistente")
+        }  
+        let controle_estoque = dado[0].Quantidade - produtoss.produtos[i].quantidade
+        function atualizar_estoque(){
+            return new Promise((resolve, reject) => {
+               conection_mysql.con.query(`update produtos set Quantidade = ${controle_estoque} where id = ${produtoss.produtos[i].idProduto}`, function(error, results){
+                    if(error){
+                        return reject (error)
+                    }return resolve(results)
+                })
+            })
+        }
+        const att = await atualizar_estoque()
+        preço_item = dado[0].Preço *produtoss.produtos[i].quantidade
+        lucrototal = lucrototal + preço_item
+        
+        function lucro_item(){
+               return new Promise((resolve, reject) => {
+                conection_mysql.con.query(`insert into financeiroo (nome, lucro_porItem) values ('${dado[0].Nome}', '${preço_item}')`, function(error, results){
+                        if(error){
+                            return reject(error)
+                        }return resolve(results)
+                    })
+                
+                 
+               }) 
+             
+            }     
+               
+            
+           const dudu = await lucro_item() 
+           function lucro_venda(){
+            return new Promise((resolve, reject) => {
+                conection_mysql.con.query(`insert into financeirooo(lucro_porVenda) values (${lucrototal}) `, function(error, results){
+                    if (error){
+                        return reject(error)
+                    }return resolve(results)
+                })
+            })
+           
+         }
+        
+        
+        
+        let dadus = await lucro_venda()
+      
+        let lista = {
+            "vendas":{
+
+            }
+        }
+      
+           
+            lista.vendas = {
+                "mensagem" : "item vendido",
+                "id_produto": produtoss.produtos[i].idProduto,
+                "nome": dado[0].Nome,
+                "Preço":dado[0].Preço,
+                "quantidade": produtoss.produtos[i].quantidade,
+                "lucro_do_produto": preço_item
+                
+            }
+            vendas.unshift(lista.vendas)
+             
+           
+    
+
+          
+        }    
+        return res.send({
+        vendas,
+        "lucrototal": lucrototal}) 
+   
+})
 
 module.exports = router
